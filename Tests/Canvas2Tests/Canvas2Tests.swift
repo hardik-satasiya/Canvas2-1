b3af7648209f099e995a2e5b085284cc789c9219
scchn <scxchn@gmail.com>
2020-08-01 11:48:56 +0800

    Fix typos

--- Tests/Canvas2Tests/Canvas2Tests.swift
+++ Tests/Canvas2Tests/Canvas2Tests.swift
@@ -5,10 +5,10 @@ final class Canvas2Tests: XCTestCase {
     
     func testPathMethod() {
         do {
-            let testMethods: [Path.Method] = [.stroke(2), .dash(1, 2, [3, 4]), .fill]
+            let testMethods: [ShapePath.Method] = [.stroke(2), .dash(1, 2, [3, 4]), .fill]
             for method in testMethods {
                 let data = try JSONEncoder().encode(method)
-                let result = try JSONDecoder().decode(Path.Method.self, from: data)
+                let result = try JSONDecoder().decode(ShapePath.Method.self, from: data)
                 XCTAssertEqual(result, method)
             }
         } catch {
@@ -19,10 +19,10 @@ final class Canvas2Tests: XCTestCase {
     func testPath() {
         let cgPath = CGMutablePath()
         cgPath.addArc(center: .zero, radius: 1, startAngle: 0, endAngle: .pi, clockwise: true)
-        let path = Path(path: cgPath, method: .stroke(1), color: .blue)
+        let path = ShapePath(path: cgPath, method: .stroke(1), color: .blue)
         do {
             let data = try JSONEncoder().encode(path)
-            let result = try JSONDecoder().decode(Path.self, from: data)
+            let result = try JSONDecoder().decode(ShapePath.self, from: data)
             XCTAssertEqual(path.cgPath.elements(), result.cgPath.elements())
             XCTAssertEqual(path.method, result.method)
             XCTAssertEqual(path.color, result.color)
@@ -37,10 +37,10 @@ final class Canvas2Tests: XCTestCase {
             .foregroundColor: NSColor.blue,
             .font: NSFont.systemFont(ofSize: 16)
         ]
-        let text = Text(string: .init(string: str, attributes: attrs), at: .zero, rotation: .pi)
+        let text = ShapeText(string: .init(string: str, attributes: attrs), at: .zero, rotation: .pi)
         do {
             let data = try JSONEncoder().encode(text)
-            let result = try JSONDecoder().decode(Text.self, from: data)
+            let result = try JSONDecoder().decode(ShapeText.self, from: data)
             XCTAssertEqual(result.string, NSAttributedString(string: str, attributes: attrs))
             XCTAssertEqual(result.point, text.point)
             XCTAssertEqual(result.angle, text.angle)
